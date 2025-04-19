4ff481344069d300330751cea391b9986abc93ba
albertkemp <albertkemp@5120L-144570-M.local>
2025-04-19 21:36:09 +1000

    dynamic routes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ['res.cloudinary.com', 'q6.itc.cn'],
     unoptimized: true,
   },
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000", "localhost:3001"],
+      allowedOrigins: ["localhost:3000", "localhost:3001", "home-cooks.netlify.app"],
     },
   },
   webpack: (config) => {
